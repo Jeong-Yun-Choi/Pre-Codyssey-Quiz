a7ed5b812a9a46e73b7567c5ce57366e588b7fc9
Jeong-Yun-Choi <jartistsherl@knou.ac.kr>
2026-08-20 21:40:56 +0900

    Feat(play-quiz): 퀴즈 풀기 기능 구현

--- src/QuizGame.py
+++ src/QuizGame.py
@@ -1,6 +1,20 @@
 from Quiz import Quiz
 
 class QuizGame:
+    # json 파일명을 상수 선언(클래스 변수) == 공유자원, 변하지 않는 파일이라고 명시
+    FILE_NAME = "state.json"
+
+    # 각자의 독립된 상태를 저장하기 위해 객체로 구현
+    # init은 초기 상태를 표현함.
+    def __init__(self):
+        # 퀴즈 목록
+        self.quizzes = []
+        # 최고 점수
+        self.best_score = 0
+
+        # json 파일 불러오기
+        self.load_data()
+        
     # ==========================
     # [기능 요구사항] 2. 메뉴 기능
     # ==========================
@@ -94,3 +108,58 @@ class QuizGame:
                 4
             ),
         ]
+
+    # ==========================
+    # [기능 요구사항] 6. 퀴즈 풀기
+    # ==========================
+    def play_quiz(self):
+
+        # 퀴즈가 없는 경우 처리
+        if len(self.quizzes) == 0:
+            print("퀴즈가 없습니다!")
+            return
+
+        # 점수 초기 상태 0으로 초기화
+        score = 0
+
+        # 총 문제 개수 출력
+        print(f"\n총 {len(self.quizzes)}문제입니다!")
+
+        # 현재 문제 번호(index), 해당되는 문제(quiz)를 순회하면서 문제번호와 문제를 함께 출력
+        for index, quiz in enumerate(self.quizzes, start=1):
+            # 문제 번호 출력
+            print(f"\n문제 {index}")
+            # 한 문제를 출력해주는 메서드로 해당되는 문제 출력
+            quiz.display()
+            # 선택지 1 ~ 4까지 입력받는 메서드 호출 
+            answer = self.get_number_input(
+                "\n정답: ",
+                1,
+                4
+            )
+
+            # 사용자가 입력한 답이 맞는지 정답 확인 메서드로 확인
+            if quiz.check_answer(answer):
+                # 정답이면 다음 명령어 출력
+                print("\n정답입니다!")
+                # 점수를 1 증가시킴(누적)
+                score += 1
+            # 아니라면 다음 명령어 출력
+            else:
+                print(
+                    f"오답입니다! (정답: {quiz.answer})"
+                )
+        # 모든 문제를 풀면 결과 표시
+        print("\n[결과]")
+        print(
+            # 총 점수를 퀴즈목록으로 나눠서 출력
+            f"{score}/{len(self.quizzes)}"
+        )
+
+        # 총 점수가 최고 점수보다 클 경우
+        if score > self.best_score:
+            # 현재 총 점수를 최고 점수에 저장(갱신)
+            self.best_score = score
+            print("축하합니다! 최고 점수를 갱신했습니다!")
+            # 게임이 끝나면 해당 내용을 저장-파일 저장하는 메서드 호출
+            self.save_data()
